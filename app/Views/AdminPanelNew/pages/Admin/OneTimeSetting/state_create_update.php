411492907dc573518ac4de3643affaa53890c43b
brillsense-shailesh <brillsense.shailesh@gmail.com>
2024-08-28 17:55:01 +0530

    other change

--- app/Views/AdminPanelNew/pages/Admin/OneTimeSetting/state_create_update.php
+++ app/Views/AdminPanelNew/pages/Admin/OneTimeSetting/state_create_update.php
@@ -8,8 +8,8 @@
     </div>
     <div class="offcanvas-body overflow-visible mt-2">
         <form id="form" method="post" enctype="multipart/form-data" class="custom-validation" action="<?= (isset($state_id) && !empty($state_id)) ? base_url(route_to('state_update_api')) : base_url(route_to('state_create_api')) ?>">
-        <input type="text" class="form-control" name="state_id" id="state_id" value="<?= @$state_id ?>"/>
-        <div class="row">
+            <input type="hidden" class="form-control" name="state_id" id="state_id" value="<?= @$state_id ?>" />
+            <div class="row">
                 <div class="col-md-12">
                     <h5>State Information</h5>
                     <div class="row">
@@ -33,13 +33,13 @@
                         </div>
 
                         <div class="mb-3 col-md-4">
-                      <label class="form-label text-capitalize">Country</label>
-                      <p class="new_form_p">Please add a Country</p>
-                      <div>
-                          <select aria-label="Default select example" id="country_id" name="country_id"></select>
-                          <span class="error-message" id="error-country_id"></span>
-                      </div>
-                  </div>>
+                            <label class="form-label text-capitalize">Country</label>
+                            <p class="new_form_p">Please add a Country</p>
+                            <div>
+                                <select aria-label="Default select example" id="country_id" name="country_id"></select>
+                                <span class="error-message" id="error-country_id"></span>
+                            </div>
+                        </div>
 
 
                         <div class="form_submit_div text-center mt-4">
@@ -53,16 +53,17 @@
 
 </div>
 <script>
-     var selected_country_id = "<?= @$country_id ?>";
+    var selected_country_id = "<?= @$country_id ?>";
 
-$(document).ready(function() {
+    $(document).ready(function() {
 
-    initializeSelectize('country_id', {
-                  placeholder: "Select Country"
-              }, "<?= base_url(route_to('country_list_api')) ?>", {}, "country_id", "country_name",
-              selected_country_id)
+        initializeSelectize('country_id', {
+                placeholder: "Select Country"
+            }, "<?= base_url(route_to('country_list_api')) ?>", {}, "country_id", "country_name",
+            selected_country_id)
 
-});
+    });
+
     function successCallback(response) {
         console.log(response);
         if (response.status == 201 || response.status == 200) {
